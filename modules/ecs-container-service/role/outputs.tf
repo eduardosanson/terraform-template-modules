215ff2d7d0956852af0439b2de3d01824066db8a
Sanson <eduardo.sanson@zoop.com.br>
2020-11-13 13:38:41 -0300

    output role/role execution name

--- modules/ecs-container-service/role/outputs.tf
+++ modules/ecs-container-service/role/outputs.tf
@@ -1,6 +1,15 @@
 output "ecs-execution-role-arn" {
   value = aws_iam_role.ecs-execution-role.arn
 }
+
+output "ecs-execution-role-name" {
+  value = aws_iam_role.ecs-execution-role.name
+}
+
 output "ecs-service-role-arn" {
   value = aws_iam_role.ecs-service-role.arn
+}
+
+output "ecs-service-role-name" {
+  value = aws_iam_role.ecs-service-role.name
 }
