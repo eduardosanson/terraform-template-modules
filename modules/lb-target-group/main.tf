4a109350c7c6068b2682e937c8a40aa916ff256f
Sanson <eduardo.sanson@zoop.com.br>
2020-11-14 17:04:17 -0300

    add health check variable dinamicaly

--- modules/lb-target-group/main.tf
+++ modules/lb-target-group/main.tf
@@ -4,14 +4,7 @@ resource "aws_lb_target_group" "target_group" {
   protocol = var.protocol != "" ? var.protocol : "HTTP"
   vpc_id   = var.vpc_id
 
-  health_check {
-    path                = var.health_check_path
-    interval            = 10
-    timeout             = 5
-    healthy_threshold   = 3
-    unhealthy_threshold = 2
-    matcher             = 200
-  }
+  health_check = var.health_check
 
   lifecycle {
     create_before_destroy = true
--- modules/lb-target-group/variable.tf
+++ modules/lb-target-group/variable.tf
@@ -2,7 +2,7 @@ variable "load_balancer_arn"{type = string}
 variable "vpc_id"           {type = string}
 variable "target_group_name"{type = string}
 variable "lb_port_redirect" {type = number}
-variable "health_check_path"{type = string}
+variable "health_check"     {}
 
 variable "protocol" {
   type = string
