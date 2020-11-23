415194d89969cb7bd5d50d02f73c282b0f6ed92d
Sanson <eduardo.sanson@zoop.com.br>
2020-11-23 17:40:07 -0300

    change public address to false on ec2 to ecs

--- modules/ecs-cluster/instances/main.tf
+++ modules/ecs-cluster/instances/main.tf
@@ -2,6 +2,7 @@ resource "aws_key_pair" "user" {
   key_name   = var.name
   public_key = file("~/.ssh/id_rsa.pub")
 }
+
 resource "aws_launch_configuration" "ec2_for_ecs" {
   name_prefix          = "ecs_instances-${terraform.workspace}"
   image_id             = var.image_id != "" ? var.image_id : data.aws_ami.ecs.id
@@ -10,7 +11,7 @@ resource "aws_launch_configuration" "ec2_for_ecs" {
   user_data            = data.template_file.user_data.rendered
   security_groups      = var.security_group_ids
   key_name             = aws_key_pair.user.key_name
-  associate_public_ip_address = true
+  associate_public_ip_address = false
 
   lifecycle {
     create_before_destroy = true
